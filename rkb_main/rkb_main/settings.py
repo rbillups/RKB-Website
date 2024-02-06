be2514742a29208d5aa536b66f27ff8c1c15e742
rbillups <reginaldbillups45@gmail.com>
2024-02-05 21:46:24 -0500

    Hosting attempt v2

--- rkb_main/rkb_main/settings.py
+++ rkb_main/rkb_main/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.jpath.join(BASE_DIR, 'staticfiles')
+STATICFILES=_DIRS=  (os.jpath.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
