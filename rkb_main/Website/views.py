e3434e0bbc549930b78f781306f605716d32dc10
Reginald Billups <keybillups@ReginaldsLaptop.attlocal.net>
2024-03-17 15:31:49 -0400

    Redesign 2

--- rkb_main/Website/views.py
+++ rkb_main/Website/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-product_type=[
+product_type = [
     {
-        'name':'Boxing Shoes',
-        'sizes':'9,9.5,10,10.5,11,12',
+        'name': 'Boxing Shoes',
+        'sizes': '9,9.5,10,10.5,11,12',
         'colorwave': 'SaSN Red and Black',
-        'cost':'$106.99',
-        'shipping':'$17',
-        'totalCost':'$124.99'
-
+        'cost': '$106.99',
+        'shipping': '$17',
+        'totalCost': '$124.99',
+        'image_path': 'website/images/Boxing_shoes.PNG'  # Note the change here
+        
     },
     {
-        'name':'Running Belt',
-        'sizes':'One size fits all',
+        'name': 'Running Belt',
+        'sizes': 'One size fits all',
         'colorwave': 'SaSN Red, Black, white',
-        'cost':'21.99',
-        'shipping':'$10',
-        'totalCost':'$31.99'
+        'cost': '$21.99',
+        'shipping': '$10',
+        'totalCost': '$31.99',
+        'image_path': 'website/images/running_belt.JPG'  # Assuming you have an image for this too
     }
-        
 ]
+
 
 # Create your views here.
 def home(request):
