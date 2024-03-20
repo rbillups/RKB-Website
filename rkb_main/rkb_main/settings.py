b69f6189f833e26bd63b1df896afcbd12f41662e
Reginald Billups <keybillups@ReginaldsLaptop.attlocal.net>
2024-03-20 19:28:44 -0400

    Meme fix

--- rkb_main/rkb_main/settings.py
+++ rkb_main/rkb_main/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 import os
+import mimetypes
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#    'whitenoise.middleware.WHiteNOiseMiddleware'
 ]
 
 ROOT_URLCONF = 'rkb_main.urls'
@@ -123,6 +126,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES=_DIRS=  (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
+mimetypes.add_type("text/css", ".css", True)
+#STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
